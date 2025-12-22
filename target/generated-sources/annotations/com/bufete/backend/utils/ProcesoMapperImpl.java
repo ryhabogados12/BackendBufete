385a6875c0718ecf5b6964a95f89839e4421a8b6
BraylexDev <perezbrayan1104@gmail.com>
2025-12-22 13:23:18 -0500

    first in this repo

--- target/generated-sources/annotations/com/bufete/backend/utils/ProcesoMapperImpl.java
+++ target/generated-sources/annotations/com/bufete/backend/utils/ProcesoMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-22T13:13:48-0500",
+    date = "2025-12-22T13:21:35-0500",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
