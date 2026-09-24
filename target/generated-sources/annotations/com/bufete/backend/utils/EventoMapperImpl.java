d5a52f55bd8579ee9d07420258e4ec5229aa3c31
ryhabogados12 <ryhabogados12@gmail.com>
2026-09-24 12:11:59 -0500

    cambios asuntos penales

--- target/generated-sources/annotations/com/bufete/backend/utils/EventoMapperImpl.java
+++ target/generated-sources/annotations/com/bufete/backend/utils/EventoMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-09-23T17:05:50-0500",
+    date = "2026-09-24T12:11:27-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
 )
 @Component
